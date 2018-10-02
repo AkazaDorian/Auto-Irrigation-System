260bdbd45aea45375e37e09c67485c3e8802c18c
Frank <FrankXu@cmail.carleton.ca>
2018-10-02 07:55:19 -0500

    Update to cuLearn version

--- src/lab4/Hello.java
+++ src/lab4/Hello.java
@@ -1,5 +1,16 @@
-package lab4;
-
 public class Hello {
 
+	public static main(String[] args) 
+	{
+		Hello hello = new Hello();
+		hello.methodA();
+		hello.methodB();
+		hello.methodC();
+	}
+
+	public void methodA(void ) { }
+
+	public void methodb(void) { }
+
+	public void methodC(void) { }
 }
